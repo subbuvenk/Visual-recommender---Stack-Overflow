d90c672d64828532cd3b4aa8757ff315c66c5b7c
Subramanian Venkateswaran <subramanian.venkateswaran@asu.edu>
2017-11-26 15:04:27 -0700

    Updated routes

--- app/routes.js
+++ app/routes.js
@@ -3,25 +3,58 @@ Maintain two indexes:
 	1. User data
 	2. Unanswered questions
 */
+//Source url: http://opensourceconnections.com/blog/2016/09/09/better-recsys-elasticsearch/
 
 // app/routes.js
 
 module.exports = function(app, client) {
 
-	//index page
+	//login endpoint
 	app.get('/', function(req, res) {
 		if (req.session && req.session.user)
 			res.redirect('/feed');
 		res.render('login.ejs', {message: ""});
 	});	
 
-	//feed page with recommendations
+	//feed endpoint
 	app.get('/feed', function(req, res) {
-		// consider user tags from user data index
-		client.search({
-
+		
+		if (!req.session || !req.session.user)
+			res.render('login.ejs', {message: "Username not selected"});
+		
+		//retrieve tags from user
+		var userTagSearch = client.search({
+			index: 'user',
+			type: 'document',
+			body: {
+				query: {
+					match : {"user" : req.session.user},
+					_source: ["tags"]
+				}
+			}
 		});
-		// search for tags_like_each in ES
+
+		var userTags;
+
+		// search for tags_like_each in ES user
+		var questionSuggestionSearch = client.search({
+			index: 'user',
+			type: 'document',
+			body: {
+				query: {
+					match: {
+						tags: userTags
+					}
+				},
+				aggregations: {
+					tags_like_user: {
+						field: tags,
+						min_doc_count: 1
+					}
+				}
+			}
+		});
+
 		/*
 		Example:
 			{
@@ -40,6 +73,12 @@ module.exports = function(app, client) {
 			    }
 			}
 		*/
+
+	//logout endpoint
+	app.get('/logout', function(req, res) {
+		// remove user from session cookie
+		//redirect to /
+	});
 
 
 	});
