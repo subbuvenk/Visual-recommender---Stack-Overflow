d54b9ac88a01d883b6a468d552e818649ae4e683
Subramanian Venkateswaran <subramanian.venkateswaran@asu.edu>
2017-11-27 16:24:17 -0700

    Created login and signup endpoints

--- app/routes.js
+++ app/routes.js
@@ -17,9 +17,43 @@ module.exports = function(app, client) {
 		res.render('login.ejs', { message: "" });
 	});
 
-	app.post('/', function(req, res) {
-
+	//login post
+	app.post('/', function(req, res, next) {
+  		passport.authenticate('local-login', function(err, user, info) {
+		    if (err) { 
+		    	return next(err); 
+		    }
+		    if (!user) { 
+		    	return res.render('login.ejs', info);
+		    }
+		    req.logIn(user, function(err) {
+			    if (err) { return next(err); }
+			    var log = new Object();
+			    log.email = user.local.email;
+			    log.action = "Login";
+			    var now = (new Date()).toJSON();
+				log.timestamp = now;
+				Login.create(log, function (error, user) {
+				if (error) {
+					console.log(error);
+				} 
+			});
+	      		return res.redirect('/profile');
+		    });
+  		})(req, res, next);
 	});
+
+	//signup get endpoint
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+	//signup post endpoint
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
 
 	//feed endpoint
 	app.get('/feed', function(req, res) {
@@ -79,13 +113,33 @@ module.exports = function(app, client) {
 			}
 		*/
 
-	//logout endpoint
-	app.get('/logout', function(req, res) {
-		// remove user from session cookie
-		//redirect to /
+	//signup form
+	app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
 
+	//signup post
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
 
+	//logout endpoint
+	app.get('/logout', function(req, res) {
+		var log = new Object();
+	    log.email = req.user.local.email;
+		req.logout();
+		if(req.user)
+			res.render('/', { message: "User data still present"});
+	    var now = (new Date()).toJSON();
+	    log.action = "Logout";
+		log.timestamp = now;
+		Login.create(log, function (error, user) {
+		if (error)
+			console.log(error);
+		});
+		res.redirect('/');
 	});
 
 	function isLoggedIn(req, res, next) {
